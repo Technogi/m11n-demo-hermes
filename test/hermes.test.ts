7da468f597570c61977fbdef24d8bb2a6c800c4f
Carlos Hernandez <carlos@technogi.com.mx>
2023-02-10 18:14:03 -0600

    Initial commit

---
+++ test/hermes.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Hermes from '../lib/hermes-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hermes-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Hermes.HermesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
